e1196b2140f4d0391c0e98687012740a84e4b659
halosatrio <63773815+halosatrio@users.noreply.github.com>
2023-08-15 15:38:04 +0700

    chore(global): migrate react vite

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,15 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
-
 module.exports = {
-  env: {
-    node: true,
-  },
   root: true,
+  env: { browser: true, es2020: true },
   extends: [
-    'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
 };
