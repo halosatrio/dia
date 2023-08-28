57437a31a939b14902df17a61aabc4b3d91ff30a
halosatrio <63773815+halosatrio@users.noreply.github.com>
2023-08-29 00:06:26 +0700

    perf: build top level await

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext', //browsers can handle the latest ES features
+  },
   plugins: [react()],
   resolve: {
     alias: {
